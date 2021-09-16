c5195c1b82a8981678dd0ead65b5cf943f346a25
Max Hatfull <max.hatfull@freeagent.com>
2021-09-16 15:56:39 +0100

    benchmark vote

--- app/controllers/voting_controller.rb
+++ app/controllers/voting_controller.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 class VotingController < ApplicationController
   # GET /show
   def show
@@ -9,24 +11,28 @@ class VotingController < ApplicationController
 
   # POST /vote
   def vote
-    loser_id = ([params[:left_id], params[:right_id]] - [params[:choice]]).first
+    Benchmark.bm do |x|
+      x.report do
+        loser_id = ([params[:left_id], params[:right_id]] - [params[:choice]]).first
 
-    #Chocolate.transaction do
-      #winner, loser = Chocolate.lock.find([params[:choice], loser_id])
-      winner, loser = Chocolate.find([params[:choice], loser_id])
-      # winner = Chocolate.find(params[:choice])
-      # loser = Chocolate.find(loser_id)
+        #Chocolate.transaction do
+        #winner, loser = Chocolate.lock.find([params[:choice], loser_id])
+        winner, loser = Chocolate.find([params[:choice], loser_id])
+        # winner = Chocolate.find(params[:choice])
+        # loser = Chocolate.find(loser_id)
 
-      winner.ranking, loser.ranking = EloService.update_rankings(
-        winner: winner.ranking,
-        loser: loser.ranking
-      )
-      winner.match_count += 1
-      loser.match_count += 1
-      winner.save!
-      loser.save!
-    #end
+        winner.ranking, loser.ranking = EloService.update_rankings(
+          winner: winner.ranking,
+          loser: loser.ranking
+        )
+        winner.match_count += 1
+        loser.match_count += 1
+        winner.save!
+        loser.save!
+        #end
 
-    redirect_to :root
+        redirect_to :root
+      end
+    end
   end
 end
