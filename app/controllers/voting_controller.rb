40a4c3cf2de38a9f97082bf78ad414888bf6bd99
Max Hatfull <max.hatfull@freeagent.com>
2021-09-16 16:07:35 +0100

    Use floats for elo service for speed

--- app/controllers/voting_controller.rb
+++ app/controllers/voting_controller.rb
@@ -1,5 +1,3 @@
-require 'benchmark'
-
 class VotingController < ApplicationController
   # GET /show
   def show
@@ -11,28 +9,21 @@ class VotingController < ApplicationController
 
   # POST /vote
   def vote
-    Benchmark.bm do |x|
-      x.report do
-        loser_id = ([params[:left_id], params[:right_id]] - [params[:choice]]).first
+    loser_id = ([params[:left_id], params[:right_id]] - [params[:choice]]).first
 
-        #Chocolate.transaction do
-        #winner, loser = Chocolate.lock.find([params[:choice], loser_id])
-        winner, loser = Chocolate.find([params[:choice], loser_id])
-        # winner = Chocolate.find(params[:choice])
-        # loser = Chocolate.find(loser_id)
+    Chocolate.transaction do
+      winner, loser = Chocolate.lock.find([params[:choice], loser_id])
 
-        winner.ranking, loser.ranking = EloService.update_rankings(
-          winner: winner.ranking,
-          loser: loser.ranking
-        )
-        winner.match_count += 1
-        loser.match_count += 1
-        winner.save!
-        loser.save!
-        #end
-
-        redirect_to :root
-      end
+      winner.ranking, loser.ranking = EloService.update_rankings(
+        winner: winner.ranking.to_f,
+        loser: loser.ranking.to_f,
+      )
+      winner.match_count += 1
+      loser.match_count += 1
+      winner.save!
+      loser.save!
     end
+
+    redirect_to :root
   end
 end
